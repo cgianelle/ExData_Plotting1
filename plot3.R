f6c16c5cd6225dc6bf710b07e356b11ecbb1a01d
Christopher Gianelle <gianelc@cisco.com>
2017-01-14 13:20:50 -0500

    Added plot3 code and PNG

--- plot3.R
+++ plot3.R
@@ -1,11 +1,11 @@
-#Plot 2
+#Plot 3
 #Read in the "Individual household electric power consumption" Data Set for dates
 #2007-02-01 and 2007-02-02.
 #
-#Create a plot of the Global Active Power over the dates
-#Save plot into file plot2.png
+#Create a plot of the Sub Metering (1-3) over the dates
+#Save plot into file plot3.png
 
-OUTPUT_FILE <- "plot2.png"
+OUTPUT_FILE <- "plot3.png"
 
 #use fread from data.table due to its speed
 library(data.table)
@@ -19,6 +19,11 @@ power <- fread("household_power_consumption.txt", na.strings = "?")
 days <- subset(power, Date == "1/2/2007" | Date == "2/2/2007")
 gap <- days$Global_active_power
 
+#Get submetering data
+sm1 <- days$Sub_metering_1
+sm2 <- days$Sub_metering_2
+sm3 <- days$Sub_metering_3
+
 #Create the date/time X axis 
 dates <- days$Date
 times <- days$Time
@@ -28,8 +33,13 @@ x <- strptime(dt, "%d/%m/%Y %H:%M:%S")
 #delete old plot file if present
 if (file.exists(OUTPUT_FILE)) file.remove(OUTPUT_FILE)
 
-#Create Plot - using a background color of gray87 to attempt to match figure/unnamed-chunk-3.png
+#Create Plot - using a background color of gray87 to attempt to match figure/unnamed-chunk-4.png
 png(file=OUTPUT_FILE, width = 480, height = 480, bg="gray87")
-plot(x, gap, type = "l", xlab="", ylab = "Global Active Power (kilowatts)")
+plot(x, sm1, type="l", ylab = "Energy sub metering", xlab = "")
+points(x, sm2, type = "l", col="red")
+points(x, sm3, type = "l", col="blue")
+legend("topright", 
+       lty=c(1,1), 
+       col=c("black","red", "blue"), 
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 dev.off()
-
