485d81e3731e5c62b3f2ed2965a78dbed3e236e7
= <=>
2017-01-14 13:26:39 -0500

    Updated comments to explain use of this

--- ds_fetcher.R
+++ ds_fetcher.R
@@ -2,6 +2,8 @@ DATA_SET <- "household_power_consumption.txt"
 DS_URL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 DS_ZIP <- "household_power_consumption.zip"
 
+#Check to see if we already have the extracted data set in our environment
+#if not, fetch the file from the URL and then extract it here
 if (!file.exists(DATA_SET)) {
     download.file(DS_URL, destfile = DS_ZIP)    
     unzip(DS_ZIP)
